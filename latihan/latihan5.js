391e9599de6e20cddfbed25f30954d51e84780cb
DiandraRifqiM <LuxamSky@gmail.com>
2023-05-25 08:05:57 +0700

    DAY-3

--- latihan/latihan5.js
+++ latihan/latihan5.js
@@ -1,12 +1,25 @@
 /**
-* Hasilkan nilai faktorial dari n
-* @namespace Latihan
-* @method factorial
-* @param {Number} n number
-* @return {Number} hasil faktorial
-*/
+ * Hasilkan nilai faktorial dari n
+ * @namespace Latihan
+ * @method factorial
+ * @param {Number} n number
+ * @return {Number} hasil faktorial
+ */
 function factorial(n) {
-    // TODO: hasilkan nilai faktorial sebagai return value
-    // apabila n < 0, maka hasilkan nilia undefined
+  // TODO: hasilkan nilai faktorial sebagai return value
+  // apabila n < 0, maka hasilkan nilia undefined
+  let num = 1;
+  for (let index = n; index > 0; index--) {
+    num = num * index;
+  }
+
+  if (n < 0) {
+    return null;
+  } else if (n == 0) {
+    return 1;
+  } else {
+    return num;
+  }
 }
+factorial(5);
 module.exports = factorial;
