4cffcb32f34e7fce1cec1d987e491eabd66d1c35
DiandraRifqiM <LuxamSky@gmail.com>
2023-05-25 07:49:55 +0700

    day-2

--- latihan/latihan4.js
+++ latihan/latihan4.js
@@ -1,14 +1,25 @@
 /**
-* Melakukan looping dari angka 1 hingga n sambil mencetak nilai loop ke terminal.
-* Menghasilkan 'Fizz' ke terminal apabila nilai dapat dibagi tiga, 'Buzz' apabila dapat dibagi 5, dan 'FizzBuzz' apabila dapat dibagi tiga dan lima.
-* Selain tiga kondisi itu, maka hasilkan nilai nya langsung.
-* @namespace Latihan
-* @method fizzBuzz
-* @param {Number} n panjang loop
-*/
+ * Melakukan looping dari angka 1 hingga n sambil mencetak nilai loop ke terminal.
+ * Menghasilkan 'Fizz' ke terminal apabila nilai dapat dibagi tiga, 'Buzz' apabila dapat dibagi 5, dan 'FizzBuzz' apabila dapat dibagi tiga dan lima.
+ * Selain tiga kondisi itu, maka hasilkan nilai nya langsung.
+ * @namespace Latihan
+ * @method fizzBuzz
+ * @param {Number} n panjang loop
+ */
 function fizzBuzz(n) {
-  // TODO: lakukan looping sebanyak n kali dan cetak nilai n 
+  // TODO: lakukan looping sebanyak n kali dan cetak nilai n
   // atau fizz, buzz, atau fizzbuzz tergantung nilai n pada setiap iterasi
+  for (let index = 1; index <= n; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (index % 3 == 0) {
+      console.log("Fizz");
+    } else if (index % 5 == 0) {
+      console.log("Buzz");
+    } else {
+      console.log(index);
+    }
+  }
 }
 
-module.exports = fizzBuzz
+module.exports = fizzBuzz;
